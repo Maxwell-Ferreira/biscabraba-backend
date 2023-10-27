36c76685af026e4d638f7f2e661ae7a94bd45904
Maxwell Ferreira <ferreiramaxwell531@gmail.com>
2023-10-27 10:10:06 -0300

    refactor: adjusts chat message listener

--- src/Listeners/Message.ts
+++ src/Listeners/Message.ts
@@ -1,25 +1,26 @@
-import { Socket } from "socket.io";
 import Game from "../Models/Game";
 import MessageRules from "../Rules/MessageRules";
-import { findGameIndexByPlayerId } from "../Utils";
 import Validator from "../Validator";
+import { ListenerProps } from "../Types/ListenerProps";
 
-const message = async (io: any, socket: Socket, games: Array<Game>, props: any) => {
-  const data = await Validator.validate(props, MessageRules);
-  if (data.errors) { return false; }
+const message = async ({ io, socket, games, data }: ListenerProps) => {
+  const payload = await Validator.validate(data, MessageRules).catch(
+    () => null
+  );
+  if (!payload) return;
 
   const text = data.text;
 
-  const gameIndex = findGameIndexByPlayerId(games, socket.id);
-  if (gameIndex !== -1) {
-    const player = games[gameIndex].getPlayerById(socket.id);
-    const playerName = player?.getName() || '';
+  const game = Game.findByPlayerId(games, socket.id);
+  if (!game) return;
 
-    const messageId = games[gameIndex].addMessage(text, playerName); 
-    const message = { playerName, text, id: messageId };
+  const player = game.getPlayerById(socket.id)!;
+  const playerName = player.getName() || "";
 
-    io.to(player?.getRoom()).emit('chat-message', message);
-  }
-}
+  const messageId = game.addMessage(text, playerName);
+  const message = { playerName, text, id: messageId };
+
+  io.to(player.getRoom()).emit("chat-message", message);
+};
 
 export default message;
