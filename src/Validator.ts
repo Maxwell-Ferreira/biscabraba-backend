86c172faa0892a2142dbc1ad4b6cfa529a2ea420
Maxwell Ferreira <ferreiramaxwell531@gmail.com>
2023-10-27 09:38:54 -0300

    refactor: add typescript to validator

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,13 +1,13 @@
 import { validateAll } from "indicative/validator";
 
 class Validator {
-  static async validate(payload, Action) {
+  static async validate<T = any>(payload: any, Action: any) {
     return validateAll(
       payload,
       Action.rules(),
       Action.messages ? Action.messages() : {},
       { removeAdditional: true }
-    );
+    ).then((data) => data as T);
   }
 }
 
