701dfbbf98511be98a7b29dffdd783e17eb08f2c
Maxwell Ferreira <ferreiramaxwell531@gmail.com>
2023-11-03 18:02:25 -0300

    refactor: removes unused code

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,26 +1,5 @@
 import { isArray } from "lodash";
 import { Socket } from "socket.io";
-import Game from "./Models/Game";
-
-export const findGame = (games: Array<Game>, idRoom: string) => games.find(game => game.getId() === idRoom);
-
-export const findGameIndex = (games: Array<Game>, idRoom: string) => games.findIndex(game => game.getId() === idRoom);
-
-export const findGameByPlayerId = (games: Array<Game>, playerId: string) => {
-  return games.find(game => {
-    let player = game.getPlayers().find(player => player.getId() === playerId);
-
-    if (player) { return player.getRoom() === game.getId(); }
-  });
-}
-
-export const findGameIndexByPlayerId = (games: Array<Game>, playerId: string) => {
-  return games.findIndex(game => {
-    let player = game.getPlayers().find(player => player.getId() === playerId);
-
-    if (player) { return player.getRoom() === game.getId(); }
-  });
-}
 
 export function emitError(socket: Socket, errors: Array<any>|string) {
   if(isArray(errors)) {
