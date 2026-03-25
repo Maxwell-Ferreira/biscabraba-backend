9129e6480552cfb72ad3656a1e240b56fcd93bcc
Maxwell Ferreira <ferreiramaxwell531@gmail.com>
2026-03-25 13:17:51 -0300

    refactor: updates frontend files

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,33 @@
-require('dotenv/config');
+require("dotenv/config");
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST"]
-  }
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-if(process.env.ENVIRONMENT === 'production') {
-  app.use(express.static(path.join(__dirname, '../../react_build')));
-  app.set('views', path.join(__dirname, '../../react_build'));
-}else {
-  app.use(express.static(path.join(__dirname, '../react_build')));
-  app.set('views', path.join(__dirname, '../react_build'));
+if (process.env.ENVIRONMENT === "production") {
+  app.use(express.static(path.join(__dirname, "../../react_build")));
+  app.set("views", path.join(__dirname, "../../react_build"));
+} else {
+  app.use(express.static(path.join(__dirname, "../react_build")));
+  app.set("views", path.join(__dirname, "../react_build"));
 }
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-
-import App from './App';
+import App from "./App";
 
 App(io);
 
 server.listen(process.env.PORT || 4000, () => {
-  console.log(`Servidor rodando na porta ${process.env.PORT ? process.env.PORT : 4000}`);
+  console.log(
+    `Servidor rodando na porta ${process.env.PORT ? process.env.PORT : 4000}`,
+  );
 });
